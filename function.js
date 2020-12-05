71229b10201d3bc7ce3f81c12776366df6c41fc3
Sahibul Nuzul Firdaus <sahibulnuzulfirdaus13@gmail.com>
2020-12-05 21:41:15 +0700

    🚀 RELEASE: tes

--- function.js
+++ function.js
@@ -4,13 +4,14 @@ const data = [
     ''
 ];
 
-    const b = linkin.innerHTML = `<a id="link"
-href="${link}"
-target="_blank"
-rel="noopener noreferrer">
-<i class="bx bxl-${icon}-alt bx-border bx-${animation}-hover"></i>
-<span>
-  ${name}
-</span></a>`;
+const b = linkin.innerHTML = `<a id="link"
+    href="${link}"
+    target="_blank"
+    rel="noopener noreferrer">
+    <i class="bx bxl-${icon}-alt bx-border bx-${animation}-hover"></i>
+    <span>
+      ${name}
+    </span>
+  </a>`;
 
 console.log(b);
